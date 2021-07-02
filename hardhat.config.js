faa89f18cf3155d72e501bd9828188d06911b812
bun919tw <bun919tw@gmail.com>
2021-07-02 21:31:57 +0800

    hardhat.config.js: update config

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,8 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-ethers');
+require('@openzeppelin/hardhat-upgrades');
 
 module.exports = {
   solidity: {
